bfe736bef206bcb8d9eece128305e573cd53c6eb
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-26 11:15:40 +0530

    added blacklist token model

---
+++ Ecommerce/Backend/models/blackListModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+const blackListTokenSchema = new mongoose.Schema({
+
+ token: {
+    type:String,
+    required: true,
+    unique: true
+ },
+ createdAt:{
+    type:Date,
+    default:Date.now
+ }
+
+})
+module.exports = mongoose.model("BlackListToken", blackListTokenSchema)
