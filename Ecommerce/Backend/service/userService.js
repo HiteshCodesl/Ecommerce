4b16403ab48f6a31d37ad6667272456585a8389a
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-26 12:13:20 +0530

    removed unwanted logs

--- Ecommerce/Backend/service/userService.js
+++ Ecommerce/Backend/service/userService.js
@@ -6,12 +6,12 @@ module.exports.createUser = async ({
     if (!username || !email || !password) {
         throw new Error("all fields are required");
     }
-console.log("content comes from usercontroller")
+
     const user = await userModel.create({
         username,
         email,
         password
     })
-    console.log("creating db creds")
+  
     return user
 }
