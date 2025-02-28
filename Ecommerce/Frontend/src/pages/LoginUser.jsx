0f8a906babd2ed6a39b08a043ca4d051170c0bb6
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-28 17:05:08 +0530

    updates the user login page

--- Ecommerce/Frontend/src/pages/LoginUser.jsx
+++ Ecommerce/Frontend/src/pages/LoginUser.jsx
@@ -38,7 +38,7 @@ setPassword('');
       <div className='h-screen w-100vw overflow-hidden bg-white'>
 
 <div className='flex gap-8 my-2'>
-  <div><i className="ri-shopping-cart-line text-black text-2xl m-3 p-3"> Shopify</i></div>
+  <div><i className="ri-shopping-cart-line text-black text-2xl m-3 p-3 font-semibold"> Shopify</i></div>
 </div>
 
 <div className='flex flex-col gap-4 relative top-20 h-full '>
@@ -49,11 +49,11 @@ setPassword('');
   }}>
 
     <div className='flex flex-col gap-4'>
-  <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="text" placeholder='Enter your Email' className='w-[90vw] border-black  border-[0.5px] bg-slate-300 text-black flex justify-center mx-auto h-12 p-5 text-lg font-sans ' />
+  <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="text" placeholder='Enter your Email' className='w-[90vw] border-black  border-[0.5px] bg-slate-200 text-black flex justify-center mx-auto h-12 p-5 text-lg font-sans ' />
 
-  <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="text" placeholder='Enter your Password' className='w-[90vw] bg-slate-300 border-black border-[0.5px] text-black flex justify-center mx-auto h-12 p-5 text-lg font-sans' />
+  <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='Enter your Password' className='w-[90vw] bg-slate-200 border-black border-[0.5px] text-black flex justify-center mx-auto h-12 p-5 text-lg font-sans' />
 
-  <button className='w-[50vw] border-black   border-[0.5px] text-black flex justify-center mx-auto h-12 py-2 text-lg font-sans  bg-slate-300 font-base'>Login</button>
+  <button className='w-[50vw] border-black   border-[0.5px] text-black flex justify-center mx-auto h-12 py-2 text-lg font-sans  bg-slate-200 font-base'>Login</button>
   </div>
   </form>
 
