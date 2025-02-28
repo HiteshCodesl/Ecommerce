4c21a77e52172fbd39d1053d533d65b6606b21a9
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-28 17:01:05 +0530

    added the start route

--- Ecommerce/Frontend/src/App.jsx
+++ Ecommerce/Frontend/src/App.jsx
@@ -4,15 +4,21 @@ import { Routes, Route } from "react-router-dom"
 import LoginUser from './pages/loginUser'
 import SignupUser from './pages/SignupUser'
 import Home from './pages/Home'
+import AdminSignup from './pages/AdminSignup'
+import AdminLogin from "./pages/AdminLogin"
+import Start from './pages/Start'
 
 function App() {
 return (
 <div>
 
 <Routes>
+<Route path='/' element={<Start />} />
 <Route path='/login' element={<LoginUser />} />
 <Route path='/signup' element={<SignupUser />} />
 <Route path='/home' element={<Home />} />
+<Route path='/admin-signup' element={<AdminSignup />}/>
+<Route path='/admin-login' element={<AdminLogin />} />
 </Routes>
 
 </div>
--- Ecommerce/Frontend/src/main.jsx
+++ Ecommerce/Frontend/src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/userContext.jsx'
+import AdminContext from './context/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <UserContext>
-     <BrowserRouter>
+    <AdminContext>
+      <UserContext>
+       <BrowserRouter>
         <App />
-     </BrowserRouter>
-   </UserContext>
- </StrictMode>,
+       </BrowserRouter>
+      </UserContext>
+    </AdminContext>
+  </StrictMode>
 )
