fbb1cf74f384cc095107b34f4a962a833a2dd874
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-02 20:25:03 +0530

    wrapped app inside product  provider

--- Ecommerce/Frontend/src/main.jsx
+++ Ecommerce/Frontend/src/main.jsx
@@ -5,16 +5,17 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/userContext.jsx'
 import AdminContext from './context/AdminContext.jsx'
+import { ProductProvider } from './context/productContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AdminContext>
       <UserContext>
-      
+      <ProductProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
-        
+          </ProductProvider>
       </UserContext>
     </AdminContext>
   </StrictMode>
