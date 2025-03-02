f91a7a309e72875cc0ece26a00413525db7bf51e
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-02 20:26:57 +0530

    moved products into productcontext

--- Ecommerce/Frontend/src/components/products.jsx
+++ Ecommerce/Frontend/src/components/products.jsx
@@ -1,61 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import { useContext } from "react";
+import {ProductContext} from "../context/productContext"
 
 export default function ProductList({popupMsg, buttonText}){
     const [isOpen, setIsOpen] = useState(false);
-
-  
-const products = [
-  {
-    productId: 1,
-    name: "Macbook M4",
-    price: "₹399999",
-    category: "Electronics",
-    image:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16-spaceblack-select-202410?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1728916322507",
-  },
-  {
-    productId: 2,
-    name: "iPhone 16 Pro Max",
-    price: "₹199999",
-    category: "Electronics",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHuYN2vuHTy4ieCIQxoMtexs_QIzRAsCAfrA&s",
-  },
-  {
-    productId: 3,
-    name: "Apple Mac Mini",
-    price: "₹199999",
-    category: "Electronics",
-    image:
-      "https://www.apple.com/newsroom/images/product/imac/standard/Apple_imac-magickeyboardnum-magicmouse2-macos-wallpaper_08042020.jpg.news_app_ed.jpg",
-  },
-  {
-    productId: 4,
-    name: "iPad Pro",
-    price: "₹99999",
-    category: "Electronics",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyXSiZS0Qa0FyiftlKyRQneCf-DTsw56JJIA&s",
-  },
-  {
-    productId: 5,
-    name: "Apple Headsets Pro",
-    price: "₹49999",
-    category: "Electronics",
-    image:
-      "https://rukminim2.flixcart.com/image/850/1000/kigbjbk0-0/headphone/p/p/i/mgyh3hn-a-apple-original-imafy8wbdnh4kbkd.jpeg?q=90&crop=false",
-  },
-  {
-    productId: 6,
-    name: "Apple Vision Pro",
-    price: "₹199999",
-    category: "Electronics",
-    image:
-      "https://www.apple.com/v/apple-vision-pro/f/images/overview/hero/portrait_base_us__fucaqiry5e2q_large.jpg",
-  },
-];
+    const { products } = useContext(ProductContext) || { products: [] }; // Prevents undefined errors
 
     useEffect(()=>{
       if(isOpen){
@@ -94,6 +44,8 @@ const products = [
 
     return(
         <div className="grid grid-cols-2 grid-flow-row gap-4 h-auto w-auto px-6 mx-auto ">
+        
+
         {products.map((product)=>(
        <div key={product.productId} className="p-4 flex flex-col items-center text-center">
         <img className="w-[100%] h-[100%] object-cover" src={product.image} alt={product.name} />
