9cf39df67039e22daeeb9481bab852e24f0330f2
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-02 13:24:35 +0530

    added cart route endpoint

--- Ecommerce/Backend/app.js
+++ Ecommerce/Backend/app.js
@@ -8,15 +8,29 @@ const connectToDb = require("./db.js")
 const userRoutes = require("./routes/userRoute.js");
 const adminRoutes = require("./routes/adminRoute.js")
 const productRoutes = require("./routes/productRoute.js")
+const cartRoutes = require("./routes/cartRoute.js")
 
 connectToDb();
 
 app.use(cookieParser())
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use("/cart", cartRoutes)
 app.use("/users",userRoutes )
 app.use("/admins", adminRoutes)
 app.use("/products", productRoutes)
+
+console.log("Registered Routes:");
+app._router.stack.forEach((r) => {
+  if (r.route) {
+    console.log(`${r.route.stack[0].method.toUpperCase()} ${r.route.path}`);
+  }
+});
+
+
 module.exports = app
+
