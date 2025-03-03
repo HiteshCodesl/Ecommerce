91c6caea865891c30cc3d1b4245d5ee290f7f264
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-03 18:08:53 +0530

    src/pages/AdminHome.jsx

--- Ecommerce/Frontend/src/pages/AdminHome.jsx
+++ Ecommerce/Frontend/src/pages/AdminHome.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { ProductContext } from '../context/productContext'
+import { useContext } from 'react'
+import AddProduct from '../components/AddProduct'
+
+
 
 function AdminHome() {
+const {products} = useContext(ProductContext)
+const [isOpen, setIsOpen] = useState(false)
+
+
   return (
     <div>
-      <div className='bg-lime-500'>Admin's-Home</div>
+
+    <div className='flex gap-6 m-5'>
+     <button onClick={()=>{
+      setIsOpen(true)
+      }} className='flex justify-center border border-black p-2 w-full font-mono text-md text-md rounded-md'>Add</button>
+     <button className='flex justify-center border border-black p-2 w-full font-mono text-md text-md rounded-md'>Update</button>
+     <button className='flex justify-center border border-black p-2 w-full font-mono text-md text-md rounded-md'>Delete</button>
     </div>
+    <div>
+
+<div className="grid grid-cols-2 grid-flow-row gap-4 h-auto w-auto px-6 mx-auto">
+
+{products.map((product)=>(
+<div key={product.productId} className="p-4 flex flex-col items-center text-center">
+<img className="w-[100%] h-[100%] object-cover" src={product.image} alt={product.name} />
+<div>{product.name}</div>
+<p className="font-semibold text-xl">{product.price}</p>
+<div className="text-lg font-medium">{product.category}</div>
+</div>
+))}
+
+
+{isOpen && <AddProduct isOpen={isOpen} setIsOpen={setIsOpen} /> }
+
+
+</div>
+
+    </div>
+   
+    </div>
+     
   )
 }
-
 export default AdminHome
