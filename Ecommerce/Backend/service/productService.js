ff5e8155ce697817c05da79b66fd50a3b984770b
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-04 20:55:34 +0530

    updated product service

--- Ecommerce/Backend/service/productService.js
+++ Ecommerce/Backend/service/productService.js
@@ -1,14 +1,14 @@
 const productModel = require("../models/productsModel")
 
 module.exports.createProduct = async({
-    name, price, category, image
+    productId, name, price, category, image
 })=>{
-    if(!name  || !price || !category || !image){
+    if(!productId || !name  || !price || !category || !image){
         throw new Error("all fields are required")
     }
 
     const product = await productModel.create({
-        name, price, category, image
+        productId, name, price, category, image
     })
 
     return product;
